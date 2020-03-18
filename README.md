59cc89d834a68ff2ce04a74c2889b1b04fd2c4e2
Simon Tucker <brotherlogic@gmail.com>
2020-03-18 16:28:51 -0700

    First pass. This closes #1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # version
+[![Coverage Status](https://coveralls.io/repos/github/brotherlogic/version/badge.svg)](https://coveralls.io/github/brotherlogic/version)
---
+++ version.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/brotherlogic/goserver"
+	"github.com/brotherlogic/keystore/client"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pbg "github.com/brotherlogic/goserver/proto"
+)
+
+//Server main server type
+type Server struct {
+	*goserver.GoServer
+}
+
+// Init builds the server
+func Init() *Server {
+	s := &Server{
+		GoServer: &goserver.GoServer{},
+	}
+	return s
+}
+
+// DoRegister does RPC registration
+func (s *Server) DoRegister(server *grpc.Server) {
+
+}
+
+// ReportHealth alerts if we're not healthy
+func (s *Server) ReportHealth() bool {
+	return true
+}
+
+// Shutdown the server
+func (s *Server) Shutdown(ctx context.Context) error {
+	return nil
+}
+
+// Mote promotes/demotes this server
+func (s *Server) Mote(ctx context.Context, master bool) error {
+	return nil
+}
+
+// GetState gets the state of the server
+func (s *Server) GetState() []*pbg.State {
+	return []*pbg.State{
+		&pbg.State{Key: "no", Value: int64(233)},
+	}
+}
+
+func main() {
+	var quiet = flag.Bool("quiet", false, "Show all output")
+	flag.Parse()
+
+	//Turn off logging
+	if *quiet {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+	server := Init()
+	server.GoServer.KSclient = *keystoreclient.GetClient(server.DialMaster)
+	server.PrepServer()
+	server.Register = server
+
+	err := server.RegisterServerV2("version", false, true)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("%v", server.Serve())
+}
